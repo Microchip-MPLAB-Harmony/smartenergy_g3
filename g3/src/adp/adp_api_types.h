8b95cb81a813f76c621eaa2f62564a8f80c82002
David Sancho <david.sancho@microchip.com>
2023-03-30 12:54:41 +0200

    SMS-3385: Added missing system include on adp_api_types.h.

--- g3/src/adp/adp_api_types.h
+++ g3/src/adp/adp_api_types.h
@@ -50,6 +50,7 @@
 // *****************************************************************************
 #include <stdint.h>
 #include <stdbool.h>
+#include "system/system.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
