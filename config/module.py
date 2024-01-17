acf2d40d3ffc54c2a822c0dbb2ec0151b814b2a2
David Sancho <david.sancho@microchip.com>
2024-01-17 15:26:20 +0100

    SMS-3821: Added SE prefix on references to Services.

--- config/module.py
+++ config/module.py
@@ -31,14 +31,14 @@ def loadModule():
         ## G3 PAL RF for WBZ45
         print("G3: Loading PAL RF for WBZ45")
         g3PalRfComponent = Module.CreateComponent("g3PalRf", "G3 PAL RF", "/SmartEnergy/G3 Stack/PAL", "pal/rf/config/pal_wbz45.py")
-        g3PalRfComponent.addCapability("g3PalRf", "G3 PAL RF", True) 
+        g3PalRfComponent.addCapability("g3PalRf", "G3 PAL RF", True)
         g3PalRfComponent.addDependency("g3PalRf_DrvRfPhy_dependency", "Device_Support", True, True)
         g3PalRfComponent.setDisplayType("G3 PAL RF")
     else:
         ## G3 PAL RF for RF215
         print("G3: Loading PAL RF for RF215")
         g3PalRfComponent = Module.CreateComponent("g3PalRf", "G3 PAL RF", "/SmartEnergy/G3 Stack/PAL", "pal/rf/config/pal_rf215.py")
-        g3PalRfComponent.addCapability("g3PalRf", "G3 PAL RF", True) 
+        g3PalRfComponent.addCapability("g3PalRf", "G3 PAL RF", True)
         g3PalRfComponent.addDependency("g3PalRf_DrvRfPhy_dependency", "DRV_RF_PHY", True, True)
         g3PalRfComponent.addDependency("g3PalRf_sysTime_dependency", "SYS_TIME", True, True)
         g3PalRfComponent.setDisplayType("G3 PAL RF")
@@ -55,18 +55,18 @@ def loadModule():
     g3ConfigComponent = Module.CreateComponent("g3_config", "G3 Stack", "/SmartEnergy/G3 Stack", "g3/config/g3_configurator.py")
     g3ConfigComponent.setDisplayType("G3 Stack")
     g3ConfigComponent.addCapability("g3StackCapability", "G3 Stack", True)
-    g3ConfigComponent.addDependency("g3_srv_random_dependency", "Random", True, True)
-    g3ConfigComponent.addDependency("g3_srv_log_report_dependency", "Log Report", True, True)
-    g3ConfigComponent.addDependency("g3_srv_security_dependency", "Security", True, True)
-    g3ConfigComponent.addDependency("g3_srv_queue_dependency", "Queue", True, True)
+    g3ConfigComponent.addDependency("g3_srv_random_dependency", "SE Random", True, True)
+    g3ConfigComponent.addDependency("g3_srv_log_report_dependency", "SE Log Report", True, True)
+    g3ConfigComponent.addDependency("g3_srv_security_dependency", "SE Security", True, True)
+    g3ConfigComponent.addDependency("g3_srv_queue_dependency", "SE Queue", True, True)
     g3ConfigComponent.addDependency("g3_palplc_dependency", "G3 PAL PLC", True, False)
     g3ConfigComponent.addDependency("g3_palrf_dependency", "G3 PAL RF", True, False)
 
-    ## ADP driver to be used as MAC interface with TCP-IP stack 
+    ## ADP driver to be used as MAC interface with TCP-IP stack
     g3MacAdpComponent = Module.CreateComponent("drvMacG3Adp", "G3ADPMAC", "/SmartEnergy/G3 Stack/", "g3/net/macg3adp/config/drv_mac_g3adp.py")
-    g3MacAdpComponent.addMultiCapability("libdrvMacG3Adp", "MAC", None)   
+    g3MacAdpComponent.addMultiCapability("libdrvMacG3Adp", "MAC", None)
     g3MacAdpComponent.addDependency("g3_adp_dependency", "G3 Stack", True, True)
-    g3MacAdpComponent.addDependency("srv_queue_dependency", "Queue", True, True)
+    g3MacAdpComponent.addDependency("srv_queue_dependency", "SE Queue", True, True)
     # TBD optional dependencies to serialization and storage
     g3MacAdpComponent.setDisplayType("MAC Layer")
-    
+
